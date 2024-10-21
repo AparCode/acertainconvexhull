4a9d251841aad4fd9093fc43adb0ab9466bfe30b
emp4506 <emp4506@rit.edu>
2024-10-21 18:15:47 -0400

    Finished the matrix operations file to do the matrix operations
    Modified the generate input file in order to also generate a matrix. Might add a "use previous matrix" TUI option later... idk yet
    
    added sample output and dataTest100 file using an identity matrix

--- data/generate_input_files.py
+++ data/generate_input_files.py
@@ -1,5 +1,5 @@
 # Input Files Generator
-# @author: Aparnaa Senthilnathan
+# @author: Aparnaa Senthilnathan, Evan Prizel
 # FOR TESTING
 # Takes two arguments, the output file name and size of the points
 # ------------------------------------------------------------------------------------------
@@ -7,20 +7,61 @@
 import sys
 import random
 
+# Generates a list of points and writes them to a file
+# Graph Range -> (-1 <= x <= 16, -2 <= y <= 5)
+# Format:
+# n
+# x1 y1
+# x2 y2
+# ...
+# xn yn
+def generatePoints(file_name, input_size):
+    P = []
+    with open(file_name, "w") as f:
+        f.write(str(input_size) + '\n')
+        for i in range(input_size):
+            x = random.uniform(-1, 16)
+            y = random.uniform(-2, 5)
+            p = (x,y)
+            if p not in P:
+                P.append(p)
+            else:
+                i -= 1
+        for p in P:
+            f.write(str(p[0]) + " " + str(p[1]) + "\n")
+        f.close()
+
+# Ask the user for input on what the matrix should be
+# and append the matrix to the file
+# Format:
+# Input Matrix:
+# m[0,0]: (user input)
+# m[0,1]: (user input)
+# m[0,2]: (user input)
+# m[1,0]: (user input)
+# m[1,1]: (user input)
+# m[1,2]: (user input)
+# The third row is always 0 0 1
+def generateMatrix(file_name):
+    m = [] * 3
+    with open(file_name, "a") as f:
+        print("Input Matrix:")
+        print("[   ] [   ] [   ]\n")
+        print("[   ] [   ] [   ]\n")
+        print("[ 0 ] [ 0 ] [ 1 ]\n")
+        for i in range(2):
+            m1 = input(f"({i},0): ")
+            m2 = input(f"({i},1): ")
+            m3 = input(f"({i},2): ")
+            f.write(m1 + " " + m2 + " " + m3 + "\n")
+            m.append([int(m1), int(m2), int(m3)])
+        f.write("0 0 1\n")
+        m.append([0, 0, 1])
+        f.close()
+    return m
+
 #graph_range -> (-1 <= x <= 16, -2 <= y <= 5)
-file_name = sys.argv[1]
+filename = sys.argv[1]
 input_size = int(sys.argv[2])
-P = []
-with open(file_name, "w") as f:
-    f.write(str(input_size) + '\n')
-    for i in range(input_size):
-        x = random.uniform(-1, 16)
-        y = random.uniform(-2, 5)
-        p = (x,y)
-        if p not in P:
-            P.append(p)
-        else:
-            i -= 1
-    for p in P:
-        f.write(str(p[0]) + " " + str(p[1]) + "\n")
-    f.close()
+P = generatePoints(filename, input_size)
+M = generateMatrix(filename)
--- matrix.py
+++ matrix.py
@@ -1,4 +1,73 @@
 # Matrix Operations Blueprint
-# @author: Aparnaa Senthilnathan, Evan Prizel
+# @Author: Aparnaa Senthilnathan, Evan Prizel
 # Computes the Matrix Operations inputted by the user
 # ------------------------------------------------------------------------------------------
+
+import sys
+
+# Takes a file with a list of point and reads it into a list P
+# input format:
+# n
+# x1 y1
+# x2 y2
+# ...
+# xn yn
+# m[1,1] m[1,2] m[1,3]
+# m[2,1] m[2,2] m[2,3]
+# m[3,1] m[3,2] m[3,3]
+def read_points_from_file(filename):
+    points = []
+    with open (filename, "r") as f:
+        # Get the number of points in the dataset
+        num_points = int(f.readline())
+        i = 0
+        matrix = []
+        for line in f:
+            if i >= num_points:
+                # Store the matrix as a 2d list of integers
+                if len(line.split()) != 3:
+                    break
+                m1, m2, m3 = line.split()
+                matrix.append([float(m1), float(m2), float(m3)])
+            else:
+                # Store the points as a tuple
+                x, y = line.split()
+                points.append((float(x), float(y)))
+            i += 1
+    return points, num_points, matrix
+
+# Takes a list of points and writes them to a file
+# Cut off after 6 decimal places
+# Format:
+# x1 y1
+# x2 y2
+# ...
+# xn yn
+def write_points_to_file(filename, points):
+    with open (filename, "w") as f:
+        for point in points:
+            f.write(f"{point[0]:.6f} {point[1]:.6f}\n")
+    return True
+
+# Takes a list of points and a matrix and returns a new list of points
+# that is the result of the matrix operation on the input points
+def matrix_operation(points, matrix):
+    new_points = []
+    for (x,y) in points:
+        new_x = matrix[0][0]*x + matrix[0][1]*y + matrix[0][2]
+        new_y = matrix[1][0]*x + matrix[1][1]*y + matrix[1][2]
+        new_points.append((new_x, new_y))
+    return new_points
+        
+# Main function
+# Must be ran with the filename of the input file as its 
+# first and only argument
+if len(sys.argv) == 2:
+    # Get the points from the file
+    points, num_of_points, matrix = read_points_from_file(sys.argv[1])
+
+    # Perform the matrix operation on the points
+    new_points = matrix_operation(points, matrix)
+
+    # Print the modified points to an output file
+    write_points_to_file("output.txt", new_points)
