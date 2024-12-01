24f4a055894679001534f5b9634bef3b29e798b0
AparCode <apar2003@gmail.com>
2024-11-30 23:57:33 -0500

    made some shapes

--- main.py
+++ main.py
@@ -6,18 +6,33 @@ import turtle
 import matrix as m 
 
 points = []
+new_points = []
 
 # function that runs when the user inputs a point
 def click(x,y):
-    points.append((x,y))
+    turtle.speed(100000000000000000000000000)
+    points.append((float(x), float(y)))
+    turtle.goto(points[0][0], points[0][1])
+    turtle.clear()
     m.write_points_to_file("input.txt", points)
-    turtle.penup()
-    if len(points) != 1:
-        turtle.pendown()
-    turtle.goto(x,y)
+    # matrix = [float(1), float(0), float(0), float(0), float(1), float(0), float(0), float(0), float(0), float(1)]
+    # m.matrix_operation(points, matrix)
     turtle.pendown()
+    if len(points) > 1:
+        for p in range(1,len(points)):
+            turtle.dot(5)
+            turtle.goto(int(points[p][0]), int(points[p][1]))
     turtle.dot(5)
-    turtle.penup()
+    turtle.goto(int(points[0][0]), int(points[0][1]))
+
+    # turtle.clear()
+    # m.write_points_to_file("input.txt", points)
+    # turtle.penup()
+    # if len(points) != 1:
+    #     turtle.pendown()
+    # turtle.pendown()
+    # turtle.dot(5)
+    # turtle.penup()
 
 turtle.ht()
 turtle.onscreenclick(click,1)
